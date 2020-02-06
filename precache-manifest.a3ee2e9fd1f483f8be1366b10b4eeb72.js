c2d62b9380dde11a85a81d0b735471a20aaf4581
Michelle Tan <mwgstan@gmail.com>
2020-02-06 14:23:50 -0600

    Updates

---
+++ precache-manifest.a3ee2e9fd1f483f8be1366b10b4eeb72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5527f7736697b51666fa7f3e2c7b13",
+    "url": "/test_page/index.html"
+  },
+  {
+    "revision": "941df7466fb3f2e5ec00",
+    "url": "/test_page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dd7a5aa20822203a111",
+    "url": "/test_page/static/js/2.ef1671de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test_page/static/js/2.ef1671de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941df7466fb3f2e5ec00",
+    "url": "/test_page/static/js/main.0efc1293.chunk.js"
+  },
+  {
+    "revision": "aba4f7c54c79996cf5f7",
+    "url": "/test_page/static/js/runtime-main.dddc3eb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_page/static/media/logo.5d5d9eef.svg"
+  }
+]);
